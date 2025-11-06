a4b2cda63fff1d49f4c4eacce577877aae226611
MarcoszT <marcostitoq@gmail.com>
2025-11-06 14:25:30 -0500

    Initial Commit

---
+++ SoundApp/SoundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundAppApp.swift
+//  SoundApp
+//
+//  Created by Marcos Tito on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
