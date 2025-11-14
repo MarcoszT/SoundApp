3a30fe338be50cae0a02c10f99839b3f3ddb755c
MarcoszT <marcostitoq@gmail.com>
2025-11-13 19:47:04 -0500

    se implementa cambios en la ui y se crea la navegacion de pantallas.

---
+++ SoundApp/Views/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  SoundApp
+//
+//  Created by Marcos Tito on 13/11/25.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
---
+++ SoundApp/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  SoundApp
+//
+//  Created by Marcos Tito on 13/11/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
